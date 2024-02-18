8cabdfce8bbcaedaefa47d7ef55b8acb0351e50a
kartavyaKloud <kartavya@kloudone.com>
2024-02-18 22:33:27 +0530

    how to add route and make your own error page show if went to any other route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { BrowserRouter , Route } from "react-router-dom"
+import { BrowserRouter , Route , Routes } from "react-router-dom"
 import Navbar from "./components/layout/Navbar"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import NotFound from "./pages/NotFound"
 export default function App() {
   return (
     <>
     <BrowserRouter>
     <div className="flex flex-col justify-between h-screen">
       <Navbar/>
-      <main>
-        content
+      <main className="border bg-lime-500">
+      <Routes>
+      <Route path="/" element = {<Home/>}/>
+      <Route path="/about" element = {<About/>}/>
+      <Route path="/notfound" element = {<NotFound/>}/>
+      <Route path="/*" element = {<NotFound/>}/>
+      </Routes>
       </main>
     </div>
     </BrowserRouter>
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1 className='text-black'>I am Not found</h1>
+    </div>
+  )
+}
+
+export default NotFound
