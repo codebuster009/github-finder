9b4cd6472e04c855aa4256c31df86242c96b2e7a
kartavyaKloud <kartavya@kloudone.com>
2024-02-26 13:20:07 +0530

    VITE env file import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
   return (
     <div>
-    <h1 className="text-6l text-blue-700">Welcome</h1>
+      <h1 className="text-6l text-blue-700">Welcome</h1>
+      {import.meta.env.VITE_APP_GITHUB_TOKEN}
+      {import.meta.env.VITE_APP_NAME}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
