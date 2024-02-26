f125ee30d61dd813676ac9cea69e096f1af8b586
kartavyaKloud <kartavya@kloudone.com>
2024-02-26 14:17:28 +0530

    after getting the data storing it in the state and mapping iver the state to show the data

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import { useEffect } from 'react'
+import { useEffect , useState } from 'react'
 const UserResults = () => {
+    const [users , setUsers] = useState([])
+    const [loading , setLoading] = useState(true)
     useEffect(()=> {
         fetchUsers();
     },[])
@@ -11,13 +13,17 @@ const UserResults = () => {
             }
         })
         const data = await response.json()
-        console.log(data)
+        setUsers(data)
+        setLoading(false)
     }
-  return (
-    <div>
-      Users results
-    </div>
-  )
+    console.log(users)
+    return (
+        <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+          {users.map((user) => (
+            <h3>{user.login}</h3>
+          ))}
+        </div>
+      )
 }
 
 export default UserResults
